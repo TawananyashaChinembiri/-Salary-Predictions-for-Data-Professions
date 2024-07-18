8ed01e60615c35dae39b389250d59d4647603e20
Tawananyasha <tawananyashachinembiri03@gmail.com>
2024-07-18 22:25:21 +0200

    Further analysis of Data Profession data

--- Data_professions_analysis.R
+++ Data_professions_analysis.R
@@ -52,3 +52,75 @@ ggsave('Designation Distribution.png', plot = designation_dist, height = 4, widt
 
 # distribution of leaves used by the data professionals
 
+leaves_plot <- ggplot(data = data, aes(x = LEAVES.USED)) +
+  geom_histogram(bins = 5, fill= 'blue', color = 'black') +
+  labs(
+    title = 'Distribution of Leaves',
+    subtitle = 'Histogram showing the distribution of Leaves for Data Professional',
+    x = 'Leave DAYS',
+    y = 'Number of Employees'
+    
+  )
+
+ggsave('Leaves plot.png', plot = leaves_plot, height = 4, width = 6)
+
+
+# distribution of units in the data industry
+units_count <- data %>% 
+  dplyr::count(UNIT)
+
+units_plot <- ggplot(data = units_count, aes(x = UNIT, y = n)) +
+  geom_bar(stat = 'identity', fill = 'blue', color = 'black') +
+  labs (
+    title = 'Units distribution',
+    subtitle = 'Bar graph showing the distribution of Units in the Data Industry',
+    x = 'UNITS',
+    y = 'Number of Employees'
+  )
+ggsave('Units distribution.png', plot = units_plot, height = 4, width = 6)
+
+# distribution of Salaries in the data industry 
+salary_plot <- ggplot(data = data, aes(x = SALARY)) +
+  geom_histogram(bins = 5 , fill = 'blue', color = 'black') +
+  labs (
+    title = 'Salary distibution',
+    subtitle = 'Bar chart showing the distribution of Salaries in the data industry',
+    x = 'SALARY' ,
+    y = 'Number of employees'
+  )
+ggsave('Salary plot.png', plot = salary_plot, height = 4, width = 6)
+
+# distribution of Data professions rating
+rating_count <- data %>% 
+  dplyr::count(RATINGS)
+
+rating_plot <- ggplot(data = rating_count, aes(x = RATINGS, y = n)) +
+  geom_bar(stat = 'identity', fill = 'blue', color = 'white') +
+  labs(
+    title = 'Ratings Distribution', 
+    subtitle = 'Histograms showing the distribution of Data Professions rating', 
+    x = 'Rating', 
+    y = 'Number of Data Professions'
+  )
+ggsave('Rating plot.png', plot = rating_plot, height = 4, width = 6)
+
+# distribution of Past experiences of the Data Professionals
+exp_count <-data %>% 
+  dplyr::count(PAST.EXP)
+
+exp_plot <- ggplot(data = exp_count, aes(x = PAST.EXP,  y = n)) +
+  geom_bar(stat = 'identity', fill = 'blue', color = 'white') +
+  labs (
+    title = 'Experience distribution',
+    subtitle = 'Bar graph showing the distribution of Data professionals Industry',
+    x = 'Past Experience',
+    y = 'Number of Employees'
+  )
+
+ggsave('Experience Plot.png', plot = exp_plot, height = 4, width = 6)
+
+# The deeper analysis on Data Professionals information
+
+# The designations that are paid more
+
+
